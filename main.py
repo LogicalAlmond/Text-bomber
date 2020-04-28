dd00b7a543f5116e356c0ddff1c849aa05c3199f
LogicalAlmond <61559365+LogicalAlmond@users.noreply.github.com>
2020-04-28 14:40:39 -0400

    New file uploaded
    Added user input functionality

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
-from time import sleep
 import pyautogui
+from time import sleep
 
-# Enable failsafe
-pyautogui.FAILSAFE = True
+print("Please open iMessage and the message thread you'd like to bomb before continuing.")
+
+# Get basic inputs from user
+sendMsg = str(input('What would you like to send?: '))
+maxSend = int(input('How many times do you want to send it?: '))
 
 # Sleep for one second
 sleep(1)
 
-# Set variables
-msg = 'txt bomb'
-sent = 0
+# Enable failsafe
+pyautogui.FAILSAFE = True
 
-# tab over to iMessge
+# Press command+tad to switch windows
 pyautogui.keyDown('command')
 pyautogui.keyDown('tab')
 pyautogui.keyUp('command')
 pyautogui.keyUp('tab')
 
-# Sleep again for 2
+# Sleep for 2 seconds
 sleep(2)
 
-# Loop it
-while sent < 10:
-    pyautogui.typewrite(msg)
+# Pause between typing for .05 seconds
+pyautogui.PAUSE = 0.05
+
+# While loops to repeatedly send the message
+count = 0
+while count < maxSend:
+    pyautogui.typewrite(sendMsg)
     pyautogui.press('enter')
-    sent += 1
+    count += 1
